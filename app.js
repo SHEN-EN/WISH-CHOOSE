0108c48e3c6a497ee381b3d992b535856456f1ba
maqishen <916862952@qq.com>
2019-06-04 10:47:44 +0800

    授权逻辑&&页面跳转

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ App({
     }),
       wx.getSetting({
         success: res => {
-          console.log(res)
           if (res.authSetting['scope.userInfo']) {
             // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
             wx.getUserInfo({
@@ -43,6 +42,10 @@ App({
                   this.userInfoReadyCallback(res)
                 }
               }
+            })
+          }else{//未授权
+            wx.redirectTo({
+              url: '../../pages/author/author',
             })
           }
         }
---
+++ pages/author/author.js
@@ -0,0 +1,62 @@
+//author.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    userInfo: {},
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+      })
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    if (e.detail.errMsg == "getUserInfo:ok") {//同意授权
+      wx.getUserInfo({
+        success: res => {
+          // 可以将 res 发送给后台解码出 unionId
+          wx.request({
+            url: 'http://10.3.13.32:5555/userInfo',
+            method: 'post',
+            data: {
+              openid: wx.getStorageSync('openid'),
+              userInfo: res.userInfo
+            },
+            success: result => {
+              //不用做任何操作测试用
+            }
+          })
+          wx.setStorage({
+            key: 'userInfo',
+            data: res.userInfo
+          })
+          // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+          // 所以此处加入 callback 以防止这种情况
+          if (this.userInfoReadyCallback) {
+            this.userInfoReadyCallback(res)
+          }
+        }
+      })
+      this.setData({
+        userInfo: e.detail.userInfo,
+      })
+      wx.redirectTo({
+        url: '../home/home',
+      })
+    }
+  }
+})
