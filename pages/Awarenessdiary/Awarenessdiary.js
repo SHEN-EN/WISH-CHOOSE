81d889f2c607d4ad59d6b48b0c8ac780df5ad321
xh <z20001119@qq.com>
2019-09-05 14:00:29 +0800

    严格模式

--- pages/Awarenessdiary/Awarenessdiary.js
+++ pages/Awarenessdiary/Awarenessdiary.js
@@ -7,52 +7,52 @@ Page({
     cardface: '../image/cardface.png',
     cardback: '../image/cardback.png'
   },
-  onLoad: function(options) {
+  onLoad: function (options) {
     // 页面创建时执行
   },
-  onShow: function() {
+  onShow: function () {
     // 页面出现在前台时执行
   },
-  onReady: function() {
+  onReady: function () {
     // 页面首次渲染完毕时执行
   },
-  onHide: function() {
+  onHide: function () {
     // 页面从前台变为后台时执行
   },
-  onUnload: function() {
+  onUnload: function () {
     // 页面销毁时执行
   },
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
     // 触发下拉刷新时执行
   },
-  onReachBottom: function() {
+  onReachBottom: function () {
     // 页面触底时执行
   },
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
     // 页面被用户分享时执行
   },
-  onPageScroll: function() {
+  onPageScroll: function () {
     // 页面滚动时执行
   },
-  onResize: function() {
+  onResize: function () {
     // 页面尺寸变化时执行
   },
   onTabItemTap(item) {
     // tab 点击时执行
-    console.log(item.index)
-    console.log(item.pagePath)
-    console.log(item.text)
+    console.log(item.index);
+    console.log(item.pagePath);
+    console.log(item.text);
   },
   // 事件响应函数
-  viewTap: function() {
+  viewTap: function () {
     this.setData({
       text: 'Set some data for updating view.'
-    }, function() {
+    }, function () {
       // this is setData callback
-    })
+    });
   },
   // 自由数据
   customData: {
     hi: 'MINA'
   }
-})
+});
